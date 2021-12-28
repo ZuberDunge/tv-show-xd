4b5c5dcd21bb870b361cb9fc74a09a5f7912a916
ZuberDunge <iamZuberDunge@gmail.com>
2021-12-28 16:28:47 +0530

    Fix : open in new tab

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,20 +7,20 @@ function Footer() {
             <div class="footer">
                 <footer>
                     <ul class="links social-links list-non-bullets">
-                        <li class="list-item-inline icon-social"><a class="link" href="https://github.com/ZuberDunge"><i
+                        <li class="list-item-inline icon-social"><a class="link" rel="noreferrer noopener" href="https://github.com/ZuberDunge"><i
                             class="fab fa-github social"></i></a></li>
                         <li class="list-item-inline icon-social"><a class="link"
-                            href="https://www.instagram.com/zuberdunge/"><i class="fab fa-instagram social"></i></a>
+                            rel="noreferrer noopener" href="https://www.instagram.com/zuberdunge/"><i class="fab fa-instagram social"></i></a>
                         </li>
                         <li class="list-item-inline icon-social"><a class="link"
-                            href="https://www.linkedin.com/in/zuberdunge/"><i class="fab fa-linkedin-in social"></i></a>
+                            rel="noreferrer noopener" href="https://www.linkedin.com/in/zuberdunge/"><i class="fab fa-linkedin-in social"></i></a>
                         </li>
                         <li class="list-item-inline icon-social"><a class="link"
-                            href="https://www.twitter.com/zuberdunge/"><i class="fab fa-twitter social"></i></a></li>
-                        <li class="list-item-inline icon-social"><a class="link" href="mailto:iamZuberDunge@gmail.com"><i
+                            rel="noreferrer noopener" href="https://www.twitter.com/zuberdunge/"><i class="fab fa-twitter social"></i></a></li>
+                        <li class="list-item-inline icon-social"><a class="link" rel="noreferrer noopener" href="mailto:iamZuberDunge@gmail.com"><i
                             class="fas fa-envelope social"></i></a></li>
                     </ul>
-                    <p class="bottom">© Made with Love 💕 by <span class="social"><a href="https://zuberdunge.tech"><span><strong>
+                    <p class="bottom">© Made with Love 💕 by <span class="social"><a rel="noreferrer noopener" href="https://zuberdunge.tech"><span><strong>
                         Zuber Dunge </strong> <i class="fas fa-external-link-alt"></i></span> </a></span>
                     </p>
                 </footer>
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
             <div className="bg-nav">
                 <div className="nav">
                     <h1 className="heading">tvShows<span>XD</span></h1>
-                    <a href="https://github.com/ZuberDunge/tv-show-xd">
+                    <a rel="noreferrer noopener" href="https://github.com/ZuberDunge/tv-show-xd">
                         <i className="fab fa-github"></i>
                         <span>
                             View on GitHub <i className="fas fa-external-link-alt"></i>
